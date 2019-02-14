9f44eeef981aca626d39abe5b76dfe95f3e019d3
João Serôdio <joaoserodio.drible@gmail.com>
2019-02-14 15:37:44 +0000

    install craft

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -52,6 +52,7 @@ class ComposerStaticInit07be5c574daeca48f078dbe14951888c
             'creocoder\\nestedsets\\' => 21,
             'craftcms\\oauth2\\client\\' => 23,
             'craft\\composer\\' => 15,
+            'craft\\ckeditor\\' => 15,
             'craft\\' => 6,
             'cebe\\markdown\\' => 14,
         ),
@@ -207,6 +208,10 @@ class ComposerStaticInit07be5c574daeca48f078dbe14951888c
         'craft\\composer\\' => 
         array (
             0 => __DIR__ . '/..' . '/craftcms/plugin-installer/src',
+        ),
+        'craft\\ckeditor\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/craftcms/ckeditor/src',
         ),
         'craft\\' => 
         array (
@@ -1966,6 +1971,10 @@ class ComposerStaticInit07be5c574daeca48f078dbe14951888c
         'craft\\behaviors\\FieldLayoutBehavior' => __DIR__ . '/..' . '/craftcms/cms/src/behaviors/FieldLayoutBehavior.php',
         'craft\\behaviors\\SessionBehavior' => __DIR__ . '/..' . '/craftcms/cms/src/behaviors/SessionBehavior.php',
         'craft\\cache\\AppPathDependency' => __DIR__ . '/..' . '/craftcms/cms/src/cache/AppPathDependency.php',
+        'craft\\ckeditor\\Field' => __DIR__ . '/..' . '/craftcms/ckeditor/src/Field.php',
+        'craft\\ckeditor\\Plugin' => __DIR__ . '/..' . '/craftcms/ckeditor/src/Plugin.php',
+        'craft\\ckeditor\\assets\\ckeditor\\CkeditorAsset' => __DIR__ . '/..' . '/craftcms/ckeditor/src/assets/ckeditor/CkeditorAsset.php',
+        'craft\\ckeditor\\assets\\field\\FieldAsset' => __DIR__ . '/..' . '/craftcms/ckeditor/src/assets/field/FieldAsset.php',
         'craft\\composer\\Factory' => __DIR__ . '/..' . '/craftcms/cms/src/composer/Factory.php',
         'craft\\composer\\Installer' => __DIR__ . '/..' . '/craftcms/plugin-installer/src/Installer.php',
         'craft\\composer\\InvalidPluginException' => __DIR__ . '/..' . '/craftcms/plugin-installer/src/InvalidPluginException.php',
